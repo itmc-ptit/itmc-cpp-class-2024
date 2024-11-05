280ea7e9e09b390de380d23c8e55a69b8d647891
nguyentantai <nguyentantai9160@gmail.com>
2024-11-05 21:21:41 +0700

    nguyentantai them bt buoi 3

---
+++ N24DCCN161/bai4/b4.cpp.cpp
@@ -0,0 +1,12 @@
+//bai4
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;//2 3
+    a=a+b;//2+3=5
+    b=a-b;//5-3=2
+    a=a-b;//5-2=3
+    cout<<a<<' '<<b;
+    return 0;
+}

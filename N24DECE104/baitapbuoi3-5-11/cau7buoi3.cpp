839efcd22703a2be77a1a3b9e6247e753f198bb7
tuanvuuuuu <tuanvu26062006@gmail.com>
2024-11-05 23:02:07 +0700

    12caubaitapbuoi3

---
+++ N24DECE104/baitapbuoi3-5-11/cau7buoi3.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=6;
+    float b=8.9777;
+    cout << a+b;
+}
---
+++ N24DECE104/baitapbuoi3-5-11/cau9buoi3.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double a=8.9999;
+    double saudauphay = a - static_cast<int>(a);
+    cout << saudauphay;
+    }

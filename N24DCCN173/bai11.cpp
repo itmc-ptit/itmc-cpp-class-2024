c5d032066a97eb46ed65155f56d316ed6bafcbaf
Phan Duc Tien <pdtien001@gmail.com>
2024-11-06 08:45:44 +0700

    phanductien them bai tap buoi 3

---
+++ N24DCCN173/bai11.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	char s; cin >> s;
+	cout << int(s) << endl;
+	return 0;
+}
---
+++ N24DCCN173/bai7.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a; cin >> a;
+	float c = float(a);
+	float b; cin >> b;
+	cout << c + b << endl;
+}

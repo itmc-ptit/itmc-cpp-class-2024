229daaf7ebe9b9fc638b438e9fbac713e4e5a964
DucNhat112 <ducnhatdn123@gmail.com>
2024-11-07 10:09:54 +0700

    Bai tap buoi 3 cua Duc Nhat

---
+++ N24DCCN147/Bài tập buổi 3/Bai 7/cau7.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int a;
+   float b;
+   cin >>a>>b;
+   cout << " Tong 2 so la: " << (float( a+b));
+  
+    return 0; 
+}
---
+++ N24DCCN147/Bài tập buổi 3/Bai 9/cau9.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float a;
+    cin >> a;
+    cout << " phan thap phan cua so la: "<< (a-(int)a);
+  
+    return 0; 
+}

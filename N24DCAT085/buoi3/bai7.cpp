26986f6d23f8899df98706353b1a79fc8c85cdc7
nguyen minh tuan <nguyentuan952006@gmail.com>
2024-11-07 17:43:03 +0700

    nguyen minh tuan b3

---
+++ N24DCAT085/buoi3/bai7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    int a;
+    float A;
+    cout <<"nhap a= ";
+    cin >>a;
+    cout <<"nhap A= ";
+    cin >>A;
+    cout <<fixed<<setprecision(4)<<a+A;
+}

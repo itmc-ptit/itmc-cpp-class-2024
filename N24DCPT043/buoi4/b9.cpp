e406798a82e5689af79a46246058b84979b4ce43
hongnhung296 <phannguyenhongnhung233@gmail.com>
2024-11-10 18:50:50 +0700

    Update b9.cpp

--- N24DCPT043/buoi4/b9.cpp
+++ N24DCPT043/buoi4/b9.cpp
@@ -10,15 +10,22 @@ int main(){
     cout << "Nhap he so c: ";
     cin >> c;
     if (a==0){
-        double x=-c/b;
-        cout << "Phuong trinh co 1 nghiem: " << x << endl;
+        if (b==0){
+            cout << "Phuong trinh vo so nghiem";
+        } else {
+            cout << "Phuong trinh vo nghiem";
+        }
     } else {
+        double x = -b/a;
+        cout << "Phuong trinh co nghiem " << x << endl;
+    } 
+    else {
         double denta=b*b-4*a*c;
         if (denta>0) {
             double x1=(-b+sqrt(denta))/2*a;
             double x2=(-b-sqrt(denta))/2*a;
             cout << "Phuong trinh co 2 nghiem phan biet: " << "x1=" << x1 << " x2=" << x2 << endl;
-        } else if (denta=0){
+        } else if (denta==0){
             double x12=-b/2*a;
             cout << "Phuong trinh cos nghiem kep: " << x12 << endl;
         } else {

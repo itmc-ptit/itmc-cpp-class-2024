072e89fa8be1b3936d04fae96e99f32e7e669c49
HuynhTanDuy <tanduyhuynh221006@gmail.com>
2024-11-11 17:51:30 +0700

    sua bai tap buoi 4

--- N24DCCN114/buoi4/bt9.cpp
+++ N24DCCN114/buoi4/bt9.cpp
@@ -7,20 +7,23 @@ int main()
     cout <<"nhap he so a, b, c cua pt ";
     cin >>a>>b>>c;
     delta = b*b - 4*a*c;
-    if ((a==0)&&(b!=0))
-        cout <<"pt co 1 nghiem x="<<-c/b;
+    if ((a==0)&&(b==0)&&(c==0))
+    cout <<"pt co vo so nghiem";
     else
-        if ((a==0)&&(b==0)||delta <0)
-            cout <<"pt vo nghiem";
-        else 
-            {
-                if(delta==0)
-                    cout <<"pt co nghiem kep x1=x2="<<-b/(2*a);
-                else 
+        if ((a==0)&&(b!=0))
+            cout <<"pt co 1 nghiem x="<<-c/b;
+        else
+            if ((a==0)&&(b==0)||delta <0)
+                cout <<"pt vo nghiem";
+            else 
                 {
-                    cout <<"pt co 2 ngiem phan biet "<<endl;
-                    cout <<"x1="<<(-b+sqrt(delta))/(2*a)<<endl;
-                    cout <<"x2="<<(-b-sqrt(delta))/(2*a)<<endl;
+                    if(delta==0)
+                        cout <<"pt co nghiem kep x1=x2="<<-b/(2*a);
+                    else 
+                    {
+                        cout <<"pt co 2 ngiem phan biet "<<endl;
+                        cout <<"x1="<<(-b+sqrt(delta))/(2*a)<<endl;
+                        cout <<"x2="<<(-b-sqrt(delta))/(2*a)<<endl;
+                    }
                 }
-            }
     }

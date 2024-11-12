35a492471e6e4205a89fc134c84afaf91f0190b5
nguyenminhtuan <nguyentuan952006@gmail.com>
2024-11-12 16:32:47 +0700

    https://github.com/itmc-ptit/itmc-cpp-class-2024.git

---
+++ N24DCAT085/buoi4/bt1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main (){
+char a; cin >>a;
+if (islower(a)) {cout<<"la chu thuong";}
+else if (isupper(a)) {cout<<"la chu hoa";}
+else {cout<<"khong xac dinh";}
+}
---
+++ N24DCAT085/buoi4/bt7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main (){
+    int a,b;
+    double x;
+    cout<<"nhap a ";
+cin >>a;
+cout<<"nhap b "; cin>>b;
+x=-b/a;
+cout<<a<<"x"<<"+"<<b<<"=0"<<endl;
+cout<<"x= ";cout<<x;
+
+}

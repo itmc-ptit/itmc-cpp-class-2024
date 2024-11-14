33018312ee543d877574e128d424737d02682c9c
evilogre1112 <phinhatlam123@gmail.com>
2024-11-14 23:30:56 +0700

    updatebaitapbuoi5

--- N24DCCN058/Baitapbuoi5/Bai4.cpp
+++ N24DCCN058/Baitapbuoi5/Bai4.cpp
@@ -4,16 +4,12 @@ using namespace std;
 int main() {
     int n ;
     cin >> n ;
-    if ( n == 1){
-        cout <<"1 khong phai la so nguyen to";
-        return 0 ; 
-    }
     for (int i = 2 ; i < sqrt(n) ; i++){
          if ( n%i == 0){
             cout << n << " khong phai la mot so nguyen to" ;
             return 0 ;
          }
     }
-    cout << n << " la mot so nguyen to" ;
+    n != 1 ? cout << n << " la mot so nguyen to" : cout << "1 khong phai la so nguyen to" ;
     return 0;
 }
--- N24DCCN058/Baitapbuoi5/Bai5.cpp
+++ N24DCCN058/Baitapbuoi5/Bai5.cpp
@@ -5,10 +5,6 @@ int main() {
     int n , dem = 0;
     bool check = true ; 
     cin >> n ;
-    if ( n == 1){
-        cout << 0;
-        return 0 ; 
-    }
     for (int i = 2 ; i < n+1 ; i++ ){
         for (int j = 2 ; j < sqrt(i) ;j++){
             if(i%j == 0){

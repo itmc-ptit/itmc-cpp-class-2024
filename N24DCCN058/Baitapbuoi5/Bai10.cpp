02ad46c11295c91be18cd730fd3e388577ac49d9
evilogre1112 <phinhatlam123@gmail.com>
2024-11-14 23:23:26 +0700

    NhatPhinopbaitapbuoi5

--- N24DCCN058/Baitapbuoi5/Bai10.cpp
+++ N24DCCN058/Baitapbuoi5/Bai10.cpp
@@ -2,15 +2,16 @@
 
 using namespace std;
 int main() {
-    int n ;
+    int n , count = 0 ;
     cin >> n ; 
     for(int i = 2 ; i < n ; i++){
-        int count = 0 ;
+        count = 0 ;
         while ( n%i == 0){
             count++ ; 
             n /= i ;
         }
         if(count != 0 ){count == 1 ? cout << i << "x" : cout << i << "^" << count << (n == 1 ? "" : "x") ;}
     }
+    if(count == 0){cout << n ; }
     return 0;
 }

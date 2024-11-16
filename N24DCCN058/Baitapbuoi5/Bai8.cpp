5ff435bd6fd33fb392e00c5d0bc45e17daf92499
evilogre1112 <phinhatlam123@gmail.com>
2024-11-16 12:31:22 +0700

    updatebaitap5hehe

--- N24DCCN058/Baitapbuoi5/Bai8.cpp
+++ N24DCCN058/Baitapbuoi5/Bai8.cpp
@@ -3,18 +3,15 @@ using namespace std;
 int main() {
     int n ;
     cin >> n ;
-    int maxDay = (n-1)*2 + 1 ;
-    int chuan = maxDay/2 + 1 ; 
-    for ( int j = 0 ; j < n ; j++){
+    int maxDay = (n-1)*2 + 1 , chuan = maxDay/2 + 1 ; 
+    for ( int j = 0 ; j <= n  ; j++){
        for (int i = 1 ; i <= maxDay ; i++){
-            int Min = chuan - j ;
-            int Max = chuan + j ; 
-            i >= Min && i <= Max ? cout << "*" : cout << " ";
+            int u = j ; 
+            if (u == n){u=0;}
+            int Min = chuan - u , Max = chuan + u ; 
+            (i >= Min && i <= Max) ? cout << "*" : cout << " ";
         }
         cout << endl ; 
-    }
-    for (int i = 1 ; i <= maxDay ; i++){
-         i == chuan ? cout << "*" : cout << " " ;
     }
     return 0;
 }
--- N24DCCN058/Baitapbuoi5/Bai9.cpp
+++ N24DCCN058/Baitapbuoi5/Bai9.cpp
@@ -3,22 +3,17 @@ using namespace std;
 int main() {
     int n , m;
     cin >> n >> m ;
-    int maxDay = (m-1)*2 + 1 ;
-    int chuan = maxDay/2 + 1 ;
-    for ( int j = 0 ; j < m ; j++){
+    int maxDay = (m-1)*2 + 1 , chuan = maxDay/2 + 1 ;
+    for ( int j = 0 ; j <= m ; j++){
         int z = 0 ; 
        for (int i = 1 ; i <= maxDay*n ; i++){
-            int Min = chuan - j ;
-            int Max = chuan + j ; 
-             i >= maxDay*z + Min && i <= maxDay*z + Max? cout << "*" : cout << " ";
+            int u = j ; 
+            if(u == m){u = 0 ;}
+            int Min = chuan - u , Max = chuan + u ; 
+            (i >= maxDay*z + Min && i <= maxDay*z + Max ) ? cout << "*" : cout << " ";
             if ( i % maxDay == 0 && z <= n){z++;}
         }
         cout << endl ; 
     }
-    int z = 0 ; 
-    for (int i = 1 ; i <= maxDay*n ; i++){
-             i == maxDay*z + chuan ? cout << "*" : cout << " ";
-            if ( i % maxDay == 0 && z <= n){z++;}
-        }
     return 0;
 }

7ed02449f3b07ab754482cef15bf2f66d18d99e8
Lê Minh Thiện <minhthienit39@gmail.com>
2024-11-21 17:04:15 +0700

    Update 6.cpp buoi 5

--- N24DCCN167/BaiTap_buoi5/6.cpp
+++ N24DCCN167/BaiTap_buoi5/6.cpp
@@ -5,7 +5,7 @@ int main()
     long long sum = 0;
     int n;
     cin >> n;
-    for (int i = 1;i < n;i++) {
+    for (int i = 1;i <= n/2;i++) {
         if (n % i == 0) {
             sum += i;
         }

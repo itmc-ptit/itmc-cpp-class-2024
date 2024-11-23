d7d4066c55acd82947807fee5dac9c7bb0e260eb
huyentrang <118000084+KRL0v0@users.noreply.github.com>
2024-11-24 00:52:46 +0700

    Update bt4.cpp

--- bt4.cpp
+++ bt4.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
+#include <cmath>  
 using namespace std;
+bool ktra(int n){
+    if (n <= 1) return false;  
+    if (n == 2) return true;  
+    for (int i = 2; i <= sqrt(n); ++i) {
+        if (n % i == 0) {
+            return false; 
+        }
+    }
+    return true;  
+}
 int main() {
-	int n,x;
-	cin>>n;
-    if (n<=1){
-    	cout<<n<<"khong phai la so nguyen to";
-	}
-	if (n>1){
-		for (x=2;x<=n-1;x++){
-			if (n%x==0){
-				cout<<n<<"khong la so nguyen to";}
-				else{cout<<n<<" la so nguyen to";}
-			}
-		}
-	}
-return 0;
+    int n;
+    cin >> n;
+    if (ktra(n)) {
+        cout << n << " la snt";
+    } else {
+        cout << n << " khong la snt";
+    }
+    return 0;
 }

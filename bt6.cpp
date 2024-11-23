3bbe4ec75257d4e03bc7b879f2a00d41e3e37f99
huyentrang <118000084+KRL0v0@users.noreply.github.com>
2024-11-24 00:59:59 +0700

    Update bt6.cpp

--- bt6.cpp
+++ bt6.cpp
@@ -3,14 +3,16 @@ using namespace std;
 int main() {
 	int n,i,tonguoc=0;
 	cin>>n;
-	for (i=2;i<=n-1;i++){
-		if (n%i!=0){
+	for (i=1;i<=n-1;i++){
+		if (n%i==0){
 			tonguoc=tonguoc+i;
 		}
 	}
-	if (tonguoc=n){
+	if (tonguoc==n){
 		cout<<n<<"la so hoan hao";
+	}
+	else{
+		cout<<n<<"khong la so hoan hao";
 	}
 return 0;
 }
-	

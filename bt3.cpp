ab909040eeaf98ecd68767612d3279988182aaaa
huyentrang <118000084+KRL0v0@users.noreply.github.com>
2024-11-25 20:55:35 +0700

    Update bt3.cpp

--- bt3.cpp
+++ bt3.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main() {
 	int i,j;
 	for (i=1;i<=9;i++){
-		cout<<"bang cuu chuong cua "<<i<<"la:";
+		cout<<"bang cuu chuong cua "<<i<<" la:"<<endl;
 		for (j=1;j<=10;j++){
-		cout<<i<<"X"<<j<<"="<<i*j;
+		cout<<i<<"X"<<j<<"="<<i*j<<endl;
 		}
+		cout<<endl;
 	}
 	return 0;
 }

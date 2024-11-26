4d7b4fb8328460da0b59f4e6c0654a4178dfff41
Lê Minh Thiện <minhthienit39@gmail.com>
2024-11-26 14:42:23 +0700

    Update bai7.cpp new

--- N24DCCN167/BaiTap_buoi4/bai7.cpp
+++ N24DCCN167/BaiTap_buoi4/bai7.cpp
@@ -5,7 +5,16 @@ int main()
     // ax + b = 0
     int a, b;
     cin >> a >> b;
-    cout << "x = " << double(-b) / a << endl;
+    if (a == 0) {
+        cout << "ko phai bac nhat.";
+    }
+    else if (b == 0) {
+        cout << "x = 0" << endl;
+    }
+    else {
+        cout << "x = " << double(-b) / a << endl;
+    }
+
   
     return 0;
 }

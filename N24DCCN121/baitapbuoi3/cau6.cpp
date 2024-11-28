38de0e99a86a08e35fe8a6a42e043041c140dfe6
nguyenhuyhoang <hn04092006@gmail.com>
2024-11-28 18:06:40 +0700

    nguyenhuyhoang sua cau 6 buoi 3

--- N24DCCN121/baitapbuoi3/cau6.cpp
+++ N24DCCN121/baitapbuoi3/cau6.cpp
@@ -4,13 +4,7 @@ using namespace std;
 
 int main(){
     double a;
-    int dem = 0;
     cin >> a;
-    double b = double(a);
-    while(b >= 1){
-        dem++;
-        b /= 10;
-    }
-    cout << setprecision(dem + 2) << a << endl;
+    cout << fixed << setprecision(2) << a << endl;
     return 0;
 }

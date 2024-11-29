3309d7522a5e07815bbd9db82f66361dc36a7aef
samho-hacker <honguyensam011106@gmail.com>
2024-11-29 18:44:43 +0700

    sua btap buoi8

--- N24DECE089/buoi8/cau3.cpp
+++ N24DECE089/buoi8/cau3.cpp
@@ -6,7 +6,7 @@ int main()
     string a , b ; 
     cout << "moi nhap chuoi 1 " ; getline(cin,a) ; 
     cout << "moi nhap chuoi 2 " ; getline(cin,b) ; 
-    if ( a == b ) { cout << "True " ; }
-    else { cout << "False " ; } 
+    if ( a == b ) { cout << "true " ; }
+    else { cout << "false " ; } 
     return 0 ; 
 }
--- N24DECE089/buoi8/cau6.cpp
+++ N24DECE089/buoi8/cau6.cpp
@@ -4,13 +4,20 @@ using namespace std ;
 int main()
 {
     string a , b  ;
-    cout << "moi nhap chuoi " ; getline(cin,a) ;
-    int c = a.size() ; 
+    cout << "moi nhap chuoi " ; getline(cin,a) ; 
+    for ( int i = 0 ; i < a.size() ; ++i ) 
+    {
+        if ( a[i] == ' ' && a[i+1] == ' ') { a.erase(i,1) ; --i ;}
+    }
+    if ( a[0] == ' ' ) { a.erase(0,1) ; }
+    if ( a[a.size()-1] == ' ' ) { a.erase(a.size()-1,1) ; }
+    cout << '[' ; 
     for ( int i = 0 ; i < a.size() ; ++i ) 
     { 
         if ( a[i] != ' ') { b += a[i]; }
-        if ( a[i] == ' ') { cout << b << endl ; b = "" ; }
-        if ( i == c - 1 ) { cout << b ; }
+        if ( a[i] == ' ') { cout << '"' << b << '"' << ',' ; b = "" ; }
+        if ( i == a.size() - 1 ) { cout << '"' << b << '"' ; }
     }
+    cout << ']' ; 
     return 0 ; 
 }

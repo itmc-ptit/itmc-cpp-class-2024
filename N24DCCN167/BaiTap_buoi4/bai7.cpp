ae2331359092c5129dadb20400ba71c8415cb348
Lê Minh Thiện <minhthienit39@gmail.com>
2024-11-29 13:36:03 +0700

    Update bai7.cpp lan 2

--- N24DCCN167/BaiTap_buoi4/bai7.cpp
+++ N24DCCN167/BaiTap_buoi4/bai7.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    // ax + b = 0
-    int a, b;
-    cin >> a >> b;
-    if (a == 0) {
-        cout << "ko phai bac nhat.";
-    }
-    else if (b == 0) {
-        cout << "x = 0" << endl;
-    }
-    else {
-        cout << "x = " << double(-b) / a << endl;
-    }
 
-  
+int main() {
+    double a, b;
+    cout << "Nhap he so a: ";
+    cin >> a;
+    cout << "Nhap he so b: ";
+    cin >> b;
+    if (a == 0) {
+        if (b == 0) {
+            cout << "Phuong trinh vo so nghiem." << endl;
+        } else {
+            cout << "Phuong trinh vo nghiem." << endl;
+        }
+    } else {
+        double x = -b / a;
+        cout << "Nghiem cua phuong trinh la: x = " << x << endl;
+    }
     return 0;
 }
